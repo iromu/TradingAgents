6aeefba8a7181c8a82c610ce6d0896f7a627f1d6
Iván Rodríguez Murillo <wantez@gmail.com>
2025-12-12 18:16:03 +0000

    Update native.sh and docker-compose.yml for Gekko service integration and configuration

--- scripts/native.sh
+++ scripts/native.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Build a native executable for this Spring application using buildpacks
-"$SCRIPT_DIR/../../mvnw" -f "$POM_FILE" -Dmaven.test.skip=true -Pnative spring-boot:build-image
+"$SCRIPT_DIR/../mvnw" -f ../pom.xml -Dmaven.test.skip=true -Pnative spring-boot:build-image
