a73afb381c606207cf465c1faaab6806f4668a4b
Iván Rodríguez Murillo <wantez@gmail.com>
2025-12-12 19:02:33 +0000

    Add scripts for building native images and update application configuration

---
+++ scripts/native-image.sh
@@ -0,0 +1,6 @@
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# Build a native executable for this Spring application using buildpacks
+"$SCRIPT_DIR/../mvnw" -f ../pom.xml -Dmaven.test.skip=true -Pnative spring-boot:build-image
+
--- scripts/native.sh
+++ scripts/native.sh
@@ -1,5 +1,17 @@
+#!/usr/bin/env bash
 # Get the directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Build a native executable for this Spring application using buildpacks
-"$SCRIPT_DIR/../mvnw" -f ../pom.xml -Dmaven.test.skip=true -Pnative spring-boot:build-image
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install java 25-graalce
+sdk use java 25-graalce
+
+chmod +x mvnw
+
+"$SCRIPT_DIR/../mvnw" --version
+native-image --version
+java -version
+
+GRAALVM_HOME=$HOME/.sdkman/candidates/java/current
+
+"$SCRIPT_DIR/../mvnw" -f ../pom.xml package -Pnative -DskipTests=true
